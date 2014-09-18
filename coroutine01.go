2519db886462008a191e28c0ccc5c490cbb4ae29
AndrewN41 <andrewn.41@gmail.com>
2014-09-18 15:31:01 -0700

    proper select form

--- coroutine01.go
+++ coroutine01.go
@@ -5,60 +5,75 @@ import (
 	"time"
 )
 
+const (
+	fillCount  = 10 // number of elements in each input channel
+	numWorkers = 3  // number of consumers.
+)
+
 func Wait() {
 	time.Sleep(2000 * time.Millisecond)
 }
 
-func Print(c chan string, d chan string, id int, done chan bool) {
-	fmt.Println("Running go-routine", id)
-	defer fmt.Println("Ending go-routine", id)
-
-loop:
-	for {
-		fmt.Print(id, " len(c): ", len(c), " \"")
-		select {
-		case str, ok := <-c:
-			fmt.Println(str, "\"")
-			if !ok {
-				break loop
-			}
-		case str, ok := <-d:
-			fmt.Println(str, "\"")
-			if !ok {
-				break loop
-			}
-		}
-
+func fillChannel(work chan string, name string) {
+	for i := 0; i < fillCount; i++ {
+		work <- fmt.Sprintf("%s%d", name, i)
 	}
-	done <- true
+	close(work) // we're finished
 }
 
-func fillChannel(ch chan string, name string) {
-	ch <- name + "0"
-	ch <- name + "1"
-	ch <- name + "2"
-	ch <- name + "3"
-	close(ch)
+func doWork(id int, ch1 chan string, ch2 chan string, done chan bool) {
+
+	fmt.Println("Running worker", id)
+	defer fmt.Println("Ending worker", id)
+
+	for ch1 != nil || ch2 != nil {
+
+		cnt1 := len(ch1)
+		cnt2 := len(ch2)
+
+		select {
+		case str, more := <-ch1:
+			if more {
+				fmt.Printf("%d: ch1(%d) %s\n", id, cnt1, str)
+			} else {
+				ch1 = nil
+				fmt.Printf("%d: ch1 closed\n", id)
+			}
+
+		case str, more := <-ch2:
+			if more {
+				fmt.Printf("%d: ch2(%d) %s\n", id, cnt2, str)
+			} else {
+				ch2 = nil
+				fmt.Printf("%d: ch2 closed\n", id)
+			}
+		}
+	}
+	done <- true
+
 }
 
 func main() {
-	numRoutines := 3
 
+	a := make(chan string, 2) // a small channel
+	b := make(chan string, 5) // a bigger channel
+
+	// generate work
+	go fillChannel(a, "A")
+	go fillChannel(b, "B")
+
+	// launch the consumers
 	done := make(chan bool)
 
-	c := make(chan string, 2)
-	d := make(chan string, 2)
-
-	go fillChannel(c, "c")
-	go fillChannel(d, "d")
-
-	fmt.Println("Before go Print(c)")
-	for i := 0; i < numRoutines; i++ {
-		go Print(c, d, i, done)
+	for i := 0; i < numWorkers; i++ {
+		go doWork(i, a, b, done)
 	}
-	fmt.Println("After go Print(c)")
-	for i := 0; i < numRoutines; i++ {
+
+	// wait for the goroutines to finish.
+	for i := 0; i < numWorkers; i++ {
 		<-done
 	}
-	fmt.Println("All goroutines done.")
+	fmt.Println("All workers done.")
+
+	Wait() // without this the defered prints from the workers doesn't flush
 }
